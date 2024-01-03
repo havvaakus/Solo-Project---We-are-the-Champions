c247ead9d85f7749a5d9f80e141f4ff40f2ecb59
Havva Kuş <havvaakus@gmail.com>
2024-01-03 21:14:02 +0300

    Added database link

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://civcik-shopping-list-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://we-are-the-champions-e979c-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const endorsementsListInDB = ref(database, "endorsements")
+const endorsementsListInDB = ref(database, "endorsementsList")
 
 const inputFieldEl = document.getElementById("input-field")
 const fromEl = document.getElementById("from-el")
 const toEl = document.getElementById("to-el")
 const publishButtonEl = document.getElementById("publish-el")
+const endorsementsListEl = document.getElementById("endorsements-list")
 
 
 publishButtonEl.addEventListener("click", function() {
@@ -22,4 +23,48 @@ publishButtonEl.addEventListener("click", function() {
     
     clearInputFieldEl()
 })
+
+onValue(endorsementsListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearEndorsementsListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToEndorsementsListEl(currentItem)
+        }    
+    } else {
+        endorsementsListEl.innerHTML = ""
+    }
+})
+
+function clearEndorsementsListEl() {
+    endorsementsListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToEndorsementsListEl(item) {
+    
+
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    // bosluk girildigi zaman bos kutu eklememesi icin
+    if (item[1].trim() === '') {
+        console.error("Add to endorsement is failed: Empty Text");
+        return;
+    }
+
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    endorsementsListEl.append(newEl)
+}
 
